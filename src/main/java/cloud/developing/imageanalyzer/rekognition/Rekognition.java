e8ef44560a2ae2efd65fd675656348b33379f3f0
adams <adam@adamsmolnik.com>
2020-05-03 18:03:16 +0200

    Add first code

---
+++ src/main/java/cloud/developing/imageanalyzer/rekognition/Rekognition.java
@@ -0,0 +1,21 @@
+package cloud.developing.imageanalyzer.rekognition;
+
+import static java.util.stream.Collectors.joining;
+
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.amazonaws.services.rekognition.model.DetectLabelsRequest;
+import com.amazonaws.services.rekognition.model.Image;
+import com.amazonaws.services.rekognition.model.S3Object;
+
+public class Rekognition {
+
+	private final AmazonRekognition rekognition = AmazonRekognitionClientBuilder.defaultClient();
+
+	public String recognize(S3Object s3Object) throws Exception {
+		var result = rekognition.detectLabels(
+				new DetectLabelsRequest().withImage(new Image().withS3Object(s3Object)).withMinConfidence(70.0f));
+		return result.getLabels().stream().map(label -> label.getName()).distinct().collect(joining(", "));
+	}
+
+}
