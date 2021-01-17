05125a6cecda60270b21e58dc2ccbd145c7944ed
adams <adam@adamsmolnik.com>
2021-01-17 13:17:00 +0100

    Change some things

--- src/main/java/cloud/developing/imageanalyzer/rekognition/Rekognition.java
+++ src/main/java/cloud/developing/imageanalyzer/rekognition/Rekognition.java
@@ -5,6 +5,7 @@ import static java.util.stream.Collectors.joining;
 import com.amazonaws.services.rekognition.AmazonRekognition;
 import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
 import com.amazonaws.services.rekognition.model.DetectLabelsRequest;
+import com.amazonaws.services.rekognition.model.DetectLabelsResult;
 import com.amazonaws.services.rekognition.model.Image;
 import com.amazonaws.services.rekognition.model.S3Object;
 
@@ -13,7 +14,7 @@ public class Rekognition {
 	private final AmazonRekognition rekognition = AmazonRekognitionClientBuilder.defaultClient();
 
 	public String recognize(S3Object s3Object) throws Exception {
-		var result = rekognition.detectLabels(
+		DetectLabelsResult result = rekognition.detectLabels(
 				new DetectLabelsRequest().withImage(new Image().withS3Object(s3Object)).withMinConfidence(70.0f));
 		return result.getLabels().stream().map(label -> label.getName()).distinct().collect(joining(", "));
 	}
